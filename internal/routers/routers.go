08cf2bb3608dc1c96400c54e60c2c790dde4d63d
Maluki Muthusi <malukimuthusi@gmail.com>
2022-05-07 13:39:25 +0300

    Add html files server endpoint

--- internal/routers/routers.go
+++ internal/routers/routers.go
@@ -1,6 +1,8 @@
 package routers
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 	"github.com/gityokie/gocrud/internal/controllers"
 	"github.com/gityokie/gocrud/internal/store"
@@ -10,6 +12,13 @@ type DebugPrintRouteFunc func(httpMethod, absolutePath, handlerName string, nuHa
 
 func SetupRouter(store store.Store, debugPrintRoute DebugPrintRouteFunc) *gin.Engine {
 	r := gin.Default()
+
+	r.LoadHTMLFiles("/home/maluki/disk2/gigs/gocrud/internal/routers/html/index.html")
+	r.GET("/index", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			"title": "Main website",
+		})
+	})
 
 	v1 := r.Group("/v1")
 	{
